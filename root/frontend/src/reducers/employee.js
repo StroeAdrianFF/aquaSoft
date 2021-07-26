a7335da412274aebbf3225c3df799c4345988e3a
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-26 18:56:42 +0300

    finished employee and root reducers

--- root/frontend/src/reducers/employee.js
+++ root/frontend/src/reducers/employee.js
@@ -1,10 +1,12 @@
 const initialState = {
+    //define initial state of employees
     employees: []
-}
+};
 
 export default function employee(state = initialState, action) {
-    switch(action.type) {
-        case "GET_EMPLOYEES":
+    //only get has payloads because it returns some data upon success
+    switch (action.type) {
+        case 'GET_EMPLOYEES':
             return {
                 ...state,
                 employees: action.payload
--- root/frontend/src/reducers/index.js
+++ root/frontend/src/reducers/index.js
@@ -2,7 +2,7 @@ import {combineReducers} from 'redux'
 import employee from './employee'
 import project from './project'
 
-export default combineReducers({
+export default combineReducers({//combine all the reducers into 1
     employee,
     project
 })
