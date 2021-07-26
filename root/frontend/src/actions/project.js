81e4308d9515b18e57262a6fb930d712ae193fc1
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-26 18:55:29 +0300

    finished project actions

--- root/frontend/src/actions/project.js
+++ root/frontend/src/actions/project.js
@@ -1,14 +1,57 @@
 import axios from 'axios';
 
-export const getProjects = () => async dispatch => {
+export const getProjects = () => async (dispatch) => {
     try {
         const res = await axios.get('http://localhost:5000/projects');
 
         dispatch({
-            type: "GET_PROJECTS",
+            type: 'GET_PROJECTS',
             payload: res.data
-        })
+        });
     } catch (error) {
-        console.log(error)
+        console.log(error);
     }
-}
+};
+
+export const addProject = (data) => async (dispatch) => {
+    try {
+        await axios.post('http://localhost:5000/insertProject', data, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        dispatch({
+            type: 'POST_PROJECT'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const updateProject = (data) => async (dispatch) => {
+    try {
+        await axios.put(`http://localhost:5000/updateProject/${data._id}`, data, {
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json'
+            }
+        });
+        dispatch({
+            type: 'UPDATE_PROJECT'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+export const deleteProject = (data) => async (dispatch) => {
+    try {
+        await axios.delete(`http://localhost:5000/deleteProject/${data._id}`);
+        dispatch({
+            type: 'DELETE_PROJECT'
+        });
+    } catch (error) {
+        console.log(error);
+    }
+};
