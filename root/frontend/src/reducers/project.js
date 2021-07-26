a06a274b7cff4ee051544bd994975c6dc50002b6
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-26 18:52:36 +0300

    updated project reducer

--- root/frontend/src/reducers/project.js
+++ root/frontend/src/reducers/project.js
@@ -1,16 +1,17 @@
 const initialState = {
-    projects: [],
-  };
-  
-  export default function project(state = initialState, action) {
+    //define initial state of projects
+    projects: []
+};
+
+export default function project(state = initialState, action) {
+    //only get has payloads because it returns some data upon success
     switch (action.type) {
-      case "GET_PROJECTS":
-        return {
-          ...state,
-          projects: action.payload,
-        };
-      default:
-        return state;
+        case 'GET_PROJECTS':
+            return {
+                ...state,
+                projects: action.payload
+            };
+        default:
+            return state;
     }
-  }
-  
+}
