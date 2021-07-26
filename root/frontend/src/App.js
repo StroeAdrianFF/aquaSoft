cec11f590f011e82092ff0912bdbd958b9129ebb
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-26 17:00:10 +0300

    Added routing to components

--- root/frontend/src/App.js
+++ root/frontend/src/App.js
@@ -1,8 +1,26 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css'; //bootstrap cdn
+import NavBar from './Components/NavBar/NavBar.jsx';
+import {BrowserRouter as Router, Redirect, Route, Switch} from 'react-router-dom'
+import MainPage from './Components/MainPage/MainPage.jsx'
+import SecondPage from './Components/SecondPage/SecondPage.jsx'
+import {Provider} from 'react-redux'
+import store from './store'
+
 
 function App() {
   return (
     <div className="App">
+      <Router>
+        <Provider store={store}>
+          <NavBar/>
+          <Switch>
+            <Route exact path="/" component={MainPage}><Redirect to="/angajati"/></Route>
+            <Route path="/angajati" component={MainPage}/>
+            <Route path="/proiecte" component={SecondPage}/>
+          </Switch>
+        </Provider>
+      </Router>
       
     </div>
   );
