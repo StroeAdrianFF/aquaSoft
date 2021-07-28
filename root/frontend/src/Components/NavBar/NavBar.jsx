1d560efe882466d70ed938588f32c632d0f82edd
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-28 10:33:04 +0300

    navbar indentation

--- root/frontend/src/Components/NavBar/NavBar.jsx
+++ root/frontend/src/Components/NavBar/NavBar.jsx
@@ -1,23 +1,24 @@
-import React from "react";
-import { Navbar, Nav, Container } from "react-bootstrap";
-import { LinkContainer } from "react-router-bootstrap"; //imported to resolve "<Link to=></Link>" from react-router-dom
+import React from 'react';
+import { Navbar, Nav, Container } from 'react-bootstrap';
+import { LinkContainer } from 'react-router-bootstrap'; //imported to resolve "<Link to=></Link>" from react-router-dom
 
 const NavBar = () => {
-  return (
-    <Navbar bg="dark" variant="dark" expand="lg">
-      <Container>
-        <Navbar.Brand >Frontend</Navbar.Brand> 
-        <Nav className="ml-auto">{/* ml-auto = margin-left: auto */}
-          <LinkContainer to="/angajati">
-            <Nav.Link>Angajati</Nav.Link>
-          </LinkContainer>
-          <LinkContainer to="/proiecte">
-            <Nav.Link>Proiecte</Nav.Link>
-          </LinkContainer>
-        </Nav>
-      </Container>
-    </Navbar>
-  );
+    return (
+        <Navbar bg='dark' variant='dark' expand='lg'>
+            <Container>
+                <Navbar.Brand>AquaSoft</Navbar.Brand>
+                <Nav className='ml-auto'>
+                    {/* ml-auto = margin-left: auto */}
+                    <LinkContainer to='/angajati'>
+                        <Nav.Link>Angajati</Nav.Link>
+                    </LinkContainer>
+                    <LinkContainer to='/proiecte'>
+                        <Nav.Link>Proiecte</Nav.Link>
+                    </LinkContainer>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
 };
 
 export default NavBar;
