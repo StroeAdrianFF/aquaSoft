29cd11890f64166b5deb70db91b3a42c10966e80
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-29 18:13:21 +0300

    updated root reducer

--- root/frontend/src/reducers/index.js
+++ root/frontend/src/reducers/index.js
@@ -1,9 +1,11 @@
 import { combineReducers } from 'redux';
 import employee from './employee';
 import project from './project';
+import user from './user';
 
 export default combineReducers({
     //combine all the reducers into 1
     employee,
-    project
+    project,
+    user
 });
