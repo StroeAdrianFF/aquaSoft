4cd42442d99b0e185209199aa7510a5daaa9b1b2
StroeAdrianFF <stroeadrian85@yahoo.com>
2021-07-30 15:12:20 +0300

    removed json stringify BECAUSE IT WAS CAUSING ERRORS

--- root/frontend/src/reducers/user.js
+++ root/frontend/src/reducers/user.js
@@ -5,10 +5,10 @@ const initialState = {
 const userReducer = (state = initialState, action) => {
     switch (action.type) {
         case 'SIGN_UP':
-            localStorage.setItem('user', JSON.stringify(action.payload));
+            localStorage.setItem('user', action.payload);
             return { ...state, authData: action.payload };
         case 'SIGN_IN':
-            localStorage.setItem('user', JSON.stringify(action.payload));
+            localStorage.setItem('user', action.payload);
             return { ...state, authData: action.payload };
         default:
             return state;
